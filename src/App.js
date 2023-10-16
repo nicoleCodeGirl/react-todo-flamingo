b84f5183096ed793dcd3541f367c10f35c79e565
nicoleCodeGirl <nicolecodegirl@gmail.com>
2023-10-15 17:34:56 -0700

    redid lesson 1-1

--- src/App.js
+++ src/App.js
@@ -6,39 +6,18 @@ const todoList = [
   { id: 3, title: "Prepare for presentation 3" }
 ];
 
-const list = document.querySelector("ul");
-
-// Map over the todoList array and generate <li> elements
-const todoItems = todoList.map(item => {
-  const listItem = document.createElement("li");
-  listItem.setAttribute("key", item.id); // Set the 'key' attribute with the 'id' property
-  listItem.textContent = item.title; // Set the text content with the 'title' property
-  return listItem;
-});
-
-// Append the generated <li> elements to the <ul>
-todoItems.forEach(item => {
-  list.appendChild(item);
-});
-
-
 
 
 function App() {
   return (
-    // <div style={{ textAlign: 'center' }}>
-    //   <header>
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <h1>
-      <ul></ul>
-    </h1>
+    <div>
+    <h1>ToDo List</h1>
+    <ul>
+      {todoList.map(item => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </ul>
+  </div>
   );
 }
 
